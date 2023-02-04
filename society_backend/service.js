b5b338d280419b698f8a2cc8c7faf6e136a18b7c
ParvPatel29 <patelparvm@gmail.com>
2023-02-04 21:52:28 +0530

    hashed password implemented using bcrypt

--- society_backend/service.js
+++ society_backend/service.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require("bcrypt")
 const app = express()
 const jwt = require('jsonwebtoken')
 const {user} = require('./db_connect')
@@ -13,32 +14,54 @@ app.use('/api',route)
 
 
 
-
 app.post('/signup',(req, res)=>{
- user.create({
-    name:req.body.name,
-    email:req.body.email,
-    password:req.body.password
- })
- .then(user=>{
-   res.json(user)
- })
- .catch(err=>{
-    res.status(400).json(err)
- })
+   const saltRounds=10
+   let password=bcrypt.hash(req.body.password,saltRounds)
+   .then(hashedPassword=>{
+        console.log(hashedPassword)
+        user.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:hashedPassword
+        })
+        .then(user=>{
+          res.json(user)
+        })
+        .catch(err=>{
+            res.status(400).json(err)
+        }) 
+   })
+   .catch(err=>{
+    console.log(err)
+   })
+  console.log(password)
+ 
 })
+
 app.post('/login',(req,res)=>{
     user.findOne({where:{email:req.body.email}}).then(user=>{
                 if(!user){
                     return res.status(400).json({ error: 'User not found' });
         }
-        if (user.password !== req.body.password) {
-        return res.status(400).json({ error: 'Incorrect password' });
-      }
+      bcrypt.compare(req.body.password, user.password)
+      .then(result => {
+        if(result){
+          const token = jwt.sign({ id: user.id }, "secret_key");
+          console.log(token)
+          res.json({ token });
+        }
+        else{
+          res.status(400).json({ error: 'Incorrect password' })
+        }
+      })
+      .catch(err => console.log(err))
+      //   if (user.password !== req.body.password) {
+      //   return res.status(400).json({ error: 'Incorrect password' });
+      // }
 
-      const token = jwt.sign({ id: user.id }, "secret_key");
-      console.log(token)
-      res.json({ token });
+      // const token = jwt.sign({ id: user.id }, "secret_key");
+      // console.log(token)
+      // res.json({ token });
     })
     .catch(error => {
       res.status(400).json({ error: 'Login failed' });
